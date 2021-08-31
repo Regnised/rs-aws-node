2929d30313e3ff4a03a7c29f95531ea6b5e85fba
regnised <regnise@gmail.com>
2021-08-31 17:30:20 +0300

    delete build folder from git

--- product-service2/.build/src/functions/getProductsList/handler.js
+++
@@ -1,9 +0,0 @@
-import { formatJSONResponse } from '@libs/apiGateway';
-import products from '../../db/products';
-const getProductsList = async (event) => {
-    console.log(`LAMBDA EVENT: ${JSON.stringify(event)}`);
-    return formatJSONResponse(products);
-};
-export default getProductsList;
-export const main = getProductsList;
-//# sourceMappingURL=handler.js.map
